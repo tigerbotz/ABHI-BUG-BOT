5b7aa51920ff743dee7f44febaaf6cdca8387911
WHITE_TIGER_BOTZ_INC <imsehpasidu@gmail.com>
2024-10-15 22:17:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919074692450"
+global.ownernomer = "94753335072"
 global.ownername = "Abhishek Suresh"
 global.ytname = "YT: Comedy Melody CH"
 global.socialm = "GitHub: AbhishekSuresh2"
